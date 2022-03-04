ef9db47121688e71577fea2e0c8c55d5baa82182
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-03-04 23:09:30 +0530

    add interactor implementation & tests

--- market/interactors/get_user_details.py
+++ market/interactors/get_user_details.py
@@ -1,8 +1,26 @@
+from market.exceptions.exceptions import UserNotFoundException
 from market.interactors.presenters.presenter_interface import PresenterInterface
+from market.interactors.storages.dtos import UserDetailsDTO
 from market.interactors.storages.user_storages_interface import UserStorageInterface
 
 
-class GetUserDetails:
-    def __init__(self, user_id: str, storage: UserStorageInterface, presenter: PresenterInterface):
-        self.user_id = user_id
+class GetUserDetailsInteractor:
+    def __init__(self, storage: UserStorageInterface):
         self.storage = storage
+
+    def get_user_details_wrapper(
+            self, user_id: str,
+            presenter: PresenterInterface
+    ):
+        response = ""
+        try:
+            user_details_dto = self._get_user_details(user_id=user_id)
+            response = presenter.get_user_details(user_details_dto=user_details_dto)
+        except UserNotFoundException:
+            response = presenter.get_user_not_found_response(user_id=user_id)
+        finally:
+            return response
+
+    def _get_user_details(self, user_id) -> UserDetailsDTO:
+        user_details_dto = self.storage.get_user(user_id=user_id)
+        return user_details_dto
