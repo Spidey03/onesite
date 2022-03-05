afcc8887506e584712e8f649964ad8eb42f253f4
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-03-05 10:25:50 +0530

    Add: urls for get site details

--- market/views/__init__.py
+++ market/views/__init__.py
@@ -1,7 +1,9 @@
+from market.views.get_site_details import get_site_details
+from market.views.get_sites import get_sites
 from market.views.get_user import get_user
 
-
 __all__ = [
-        'get_user'
+    'get_user',
+    'get_site_details',
+    'get_sites'
 ]
-
