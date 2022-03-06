bb9eb2227a622c1fdd6caa180ff8ad026c5c2055
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-03-06 21:59:06 +0530

    Get Site Details Bulk

--- market/urls.py
+++ market/urls.py
@@ -5,5 +5,5 @@ from market import views
 urlpatterns = [
     path('get_user/<str:id>', views.get_user, name="Get User"),
     path('get_site_details/<str:id>', views.get_site_details, name="Get Site Details"),
-    path('get_sites', views.get_sites, name="Get List of Sites")
+    path('get_sites', views.get_sites_bulk, name="Get List of Sites")
 ]
--- market/views/__init__.py
+++ market/views/__init__.py
@@ -1,9 +1,9 @@
 from market.views.get_site_details import get_site_details
-from market.views.get_sites import get_sites
+from market.views.get_sites_bulk import get_sites_bulk
 from market.views.get_user import get_user
 
 __all__ = [
     'get_user',
     'get_site_details',
-    'get_sites'
+    'get_sites_bulk'
 ]
