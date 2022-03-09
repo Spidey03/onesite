5e2057aff9fb9438eccdfdda04d6777f5113174b
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-03-09 10:56:33 +0530

    fix url path_parameter

--- market/urls.py
+++ market/urls.py
@@ -4,6 +4,6 @@ from market import views
 
 urlpatterns = [
     path('get_user/<str:id>', views.get_user, name="Get User"),
-    path('get_site_details/<str:id>', views.get_site_details, name="Get Site Details"),
+    path('get_site_details/<str:site_id>', views.get_site_details, name="Get Site Details"),
     path('get_sites', views.get_sites_bulk, name="Get List of Sites")
 ]
