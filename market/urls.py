392ec1b43aa7a7bd88c16c81b33f6cd3ac61ebab
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-04-13 09:38:47 +0530

    get user profile details

--- market/urls.py
+++ market/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from market import views
 
 urlpatterns = [
+    path('get_user/me', views.get_profile, name="Get Profile"),
     path('get_user/<str:id>', views.get_user, name="Get User"),
     path('get_site_details/<str:site_id>', views.get_site_details, name="Get Site Details"),
     path('get_sites', views.get_sites_bulk, name="Get List of Sites"),
--- market/views/get_user.py
+++ market/views/get_user.py
@@ -15,3 +15,18 @@ def get_user(request, id: str):
 
     response = interactor.get_user_details_wrapper(user_id=id, presenter=presenter)
     return Response(response)
+
+@api_view(['GET'])
+def get_user(request):
+    user_id = request.data.get('user_id')
+    from market.storages.user_storage_implementation import UserStorageImplementation
+    storage = UserStorageImplementation()
+
+    from market.presenters.presenter_implementation import PresenterImplementation
+    presenter = PresenterImplementation()
+
+    from market.interactors.get_user_details import GetUserDetailsInteractor
+    interactor = GetUserDetailsInteractor(storage=storage)
+
+    response = interactor.get_user_details_wrapper(user_id=user_id, presenter=presenter)
+    return Response(response)
