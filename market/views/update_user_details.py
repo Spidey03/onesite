ac6bb87e218ec201e3706f3c1944892ff975f386
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-04-30 18:08:46 +0530

    change API method to UPDATE

--- market/views/update_user_details.py
+++ market/views/update_user_details.py
@@ -1,6 +1,3 @@
-import datetime
-import uuid
-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -17,7 +14,7 @@ def get_user_details_dto(data):
     )
 
 
-@api_view(["POST"])
+@api_view(["UPDATE"])
 def update_user_details(request):
     from market.serializers.user_serializer import UserSerializer
     serializer = UserSerializer(data=request.data)
