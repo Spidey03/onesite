bae91c43d6869339e66c9aa967a05a91d64168ed
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-05-24 17:20:03 +0530

    update model <-

--- market/models/user.py
+++ market/models/user.py
@@ -6,10 +6,6 @@ from market.common import get_uuid
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=get_uuid())
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100, blank=True, null=True)
-    middle_name = models.CharField(max_length=100, blank=True, null=True)
-    joined_at = models.DateTimeField(blank=True, null=True)
     mobile_number = models.CharField(max_length=12, unique=True)
     email = models.CharField(unique=True, max_length=30)
 
