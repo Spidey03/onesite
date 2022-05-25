bdb3f8e13dcc387a1f7fa79c9494ebe4afbea581
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-05-25 16:06:14 +0530

    Update models

--- market/models/user.py
+++ market/models/user.py
@@ -5,9 +5,11 @@ from market.common import get_uuid
 
 
 class User(AbstractUser):
-    id = models.UUIDField(primary_key=True, default=get_uuid())
+    id = models.UUIDField(primary_key=True)
     mobile_number = models.CharField(max_length=12, unique=True)
     email = models.CharField(unique=True, max_length=30)
 
+    REQUIRED_FIELDS = ['id', 'first_name', 'last_name', 'email', 'mobile_number']
+
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f'{self.first_name} {self.last_name}'
