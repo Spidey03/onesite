3ff8189b07317d46a14ddff56a0dd937c0180761
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-05-25 12:48:21 +0530

    Create Refresh Token Storage Implementation

--- common/storage_implementation/dtos.py
+++ common/storage_implementation/dtos.py
@@ -4,3 +4,11 @@ from dataclasses import dataclass
 @dataclass
 class ApplicationDTO:
     id: str
+
+
+@dataclass
+class RefreshTokenDTO:
+    token: str
+    access_token: str
+    user_id: str
+    revoked: bool
--- common/storage_implementation/oauth2_storage_implementation.py
+++ common/storage_implementation/oauth2_storage_implementation.py
@@ -1,8 +1,8 @@
-from common.storage_implementation.dtos import ApplicationDTO
+from common.storage_implementation.dtos import ApplicationDTO, RefreshTokenDTO
 
 
 class Oauth2StorageImplementation:
-    def get_or_create_default_application(self, user_id) -> (ApplicationDTO, bool):
+    def get_or_create_default_application(self, user_id: str) -> (ApplicationDTO, bool):
         from django.conf import settings
         from oauth2_provider.models import Application
         from oauth2_provider.models import AbstractApplication
@@ -27,3 +27,32 @@ class Oauth2StorageImplementation:
     @staticmethod
     def _create_application_dto(application):
         return ApplicationDTO(id=application.id)
+
+    def create_refresh_token(
+        self, user_id: str, application_id: str, access_token_id: str
+    ) -> RefreshTokenDTO:
+        from oauth2_provider.models import RefreshToken
+
+        refresh_token = self._generate_token()
+        refresh_token_obj = RefreshToken.objects.create(
+            user_id=user_id,
+            token=refresh_token,
+            access_token=access_token_id,
+            application_id=application_id,
+        )
+
+        return RefreshTokenDTO(
+            token=refresh_token_obj.token,
+            access_token=refresh_token_obj.access_token,
+            user_id=refresh_token_obj.user_id,
+            revoked=refresh_token_obj.revoked,
+        )
+
+    @staticmethod
+    def _generate_token():
+        import string
+        import random
+
+        size = 30
+        chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
+        return ''.join(random.choice(chars) for _ in range(size))
