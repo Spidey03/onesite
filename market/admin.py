ccdcaa51205d0ef122e685e0bce250b585a28ffc
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-06-02 21:18:03 +0530

    DB Actions populate

--- market/admin.py
+++ market/admin.py
@@ -67,7 +67,7 @@ def discard(modeladmin, request, queryset):
     from market.constants.constants import ActionStatusChoices
 
     for action_item in queryset:
-        print(action_item.status)
+
         if action_item.status not in ['IN PROGRESS', 'DISCARDED', 'DONE', 'FAILED']:
             action_item.status = ActionStatusChoices.DISCARDED.value
             action_item.save()
@@ -77,12 +77,41 @@ def discard(modeladmin, request, queryset):
 discard.short_description = 'Discard selected action items'
 
 
+def populate(modeladmin, request, queryset):
+    from market.constants.constants import ActionStatusChoices
+
+    for action_item in queryset:
+        if action_item.status not in ['IN PROGRESS', 'DISCARDED', 'DONE', 'FAILED']:
+            action_item.status = ActionStatusChoices.PENDING.value
+            action_item.save()
+            try:
+                _populate_users()
+            except Exception as e:
+                action_item.description = e.__class__
+                action_item.message = f'{str(e)}'
+                action_item.status = ActionStatusChoices.FAILED.value
+            else:
+                action_item.description = 'Populate Successfully'
+                action_item.status = ActionStatusChoices.DONE.value
+            action_item.save()
+
+
+populate.short_description = 'Populate Data'
+
+
+def _populate_users():
+    from market.utils.populate.populate_users import PopulateUsers
+
+    o = PopulateUsers()
+    o.populate()
+
+
 @admin.register(DBAction)
 class DBActionAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'status')
     list_filter = ('status',)
     fields = ('name', 'description', 'status', 'message')
-    actions = [discard]
+    actions = [discard, populate]
 
     def get_readonly_fields(self, request, obj=None):
         if obj:
