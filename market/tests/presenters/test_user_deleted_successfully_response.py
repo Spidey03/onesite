b414d448203fd50f2cac9b70700f21f1dc93a5bc
Naveen Kumar <sayhai2naveenkumar@gmail.com>
2022-06-04 21:51:22 +0530

    Revert "Remove User"

--- market/tests/presenters/test_user_deleted_successfully_response.py
+++ market/tests/presenters/test_user_deleted_successfully_response.py
@@ -12,7 +12,7 @@ class TestDeleteUserSuccessfullyResponse:
         # Arrange
         expected_response = {
             'res_status': 'USER_DELETE_SUCCESSFULLY',
-            'response': 'User account removed successfully, you can recover your account when you needed',
+            'response': 'User account removed successfully',
             'status_code': 200,
         }
         # Act
